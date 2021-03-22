6473c4d105d0c9b5747d3d64670b2285fffe6a85
aakashjosh <67766038+aakashjosh@users.noreply.github.com>
2021-03-23 00:58:07 +0530

    Delete java/com/niit/mountaingearcart directory

--- java/com/niit/mountaingearcart/srv/maildetails_srv.java
+++
@@ -1,7 +0,0 @@
-package com.niit.mountaingearcart.srv;
-
-import com.niit.mountaingearcart.model.maildetails;
-
-public interface maildetails_srv {
-	public void savedetails(maildetails maildetails);
-}
--- java/com/niit/mountaingearcart/srv/onlinetransaction_srv.java
+++
@@ -1,9 +0,0 @@
-package com.niit.mountaingearcart.srv;
-
-import com.niit.mountaingearcart.model.onlinetransaction;
-
-public interface onlinetransaction_srv {
-	public void savetrasaction(onlinetransaction onlinetransaction);
-	
-	public void blocktransaction(String user_name);
-}
